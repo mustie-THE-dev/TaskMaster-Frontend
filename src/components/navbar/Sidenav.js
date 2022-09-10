3cc72db0557e89d7857775cd086de568c4663717
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-10 15:30:22 +0300

    finished working on and styling the sidenav

--- src/components/navbar/Sidenav.js
+++ src/components/navbar/Sidenav.js
@@ -17,5 +17,99 @@ const Navigation = ({ projects, favorites, mode, fetchProjects }) => {
     history.push('/projects')
     fetchProjects()
   }
+  return (
+    <>
+      <Toolbar className='flex'>
+        <a onClick={handleHomeClick} href='#' className='flex link'>
+          <Logo />
+          <Typography
+            className='letter-spacing'
+            component='h1'
+            variant='h6'
+            noWrap
+            sx={{ color: mode ? '#444' : '#fff' }}
+            align='center'>
+            Mercury
+          </Typography>
+        </a>
+      </Toolbar>
+      <FavoriteList list={favorites} />
+
+      <Divider sx={{ display: { sm: 'none' } }} />
+
+      <ProjectList list={projects} />
+    </>
+  )
+}
+
+const SideNav = ({
+    open,
+    toggleDrawer,
+    window,
+    projects,
+    mode,
+    fetchProjects,
+  }) => {
+    const container =
+      window !== undefined ? () => window().document.body : undefined
+  
+    const favorites = projects
+      ? projects.filter((project) => project.favorite === true)
+      : []
+  
+    return (
+      <>
+        {/* mobile */}
+        <Drawer
+          container={container}
+          variant='temporary'
+          open={open}
+          onClose={toggleDrawer}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}>
+          <Navigation
+            projects={projects}
+            favorites={favorites}
+            mode={mode}
+            fetchProjects={fetchProjects}
+          />
+        </Drawer>
+  
+        {/* desktop */}
+        <Drawer
+          variant='permanent'
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+              border: 'none',
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.background.default
+                  : theme.palette.grey[900],
+            },
+          }}
+          open>
+          <Navigation
+            projects={projects}
+            favorites={favorites}
+            mode={mode}
+            fetchProjects={fetchProjects}
+          />
+        </Drawer>
+      </>
+    )
+  }
+  
+  export default SideNav
 
   
