3a3aeb9e488bed3105666473b7a1817ec47f334e
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-10 15:35:08 +0300

    created an event that handles color schemes for the project lists

--- src/components/navbar/Topnavbar.js
+++ src/components/navbar/Topnavbar.js
@@ -1,20 +1,20 @@
-import * as React from 'react'
-import '../../css/topnav.css'
-import { Logo } from './Logo'
-import MuiAppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Badge from '@mui/material/Badge'
-import Avatar from '@mui/material/Avatar'
-import TextField from '@mui/material/TextField'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined'
-import SearchIcon from '@mui/icons-material/Search'
-import Brightness2Icon from '@mui/icons-material/Brightness2'
-import Brightness7Icon from '@mui/icons-material/Brightness7'
+import * as React from "react";
+import "../../css/topnav.css";
+import { Logo } from "./Logo";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import Brightness2Icon from "@mui/icons-material/Brightness2";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
-const drawerWidth = 240
+const drawerWidth = 240;
 
 const TopNav = ({
   open,
@@ -24,53 +24,58 @@ const TopNav = ({
   search,
   setSearch,
 }) => {
-  const icon = !mode ? <Brightness7Icon /> : <Brightness2Icon />
+  const icon = !mode ? <Brightness7Icon /> : <Brightness2Icon />;
 
   return (
     <MuiAppBar
-      color='transparent'
+      color="transparent"
       enableColorOnDark
-      position='fixed'
+      position="fixed"
       open={open}
       elevation={0}
       sx={{
         width: { sm: `calc(100% - ${drawerWidth}px)` },
         ml: { sm: `${drawerWidth}px` },
-      }}>
-      <Toolbar className='flex'>
+      }}
+    >
+      <Toolbar className="flex">
         <IconButton
-          edge='start'
-          color='inherit'
-          aria-label='open drawer'
-          sx={{ mr: 0, display: { sm: 'none' } }}
-          onClick={toggleDrawer}>
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 0, display: { sm: "none" } }}
+          onClick={toggleDrawer}
+        >
           <MenuIcon />
         </IconButton>
         <Typography
-          component='h1'
-          variant='h6'
-          color='inherit'
+          component="h1"
+          variant="h6"
+          color="inherit"
           noWrap
-          align='center'
-          sx={{ display: { xs: 'block', sm: 'none' } }}>
+          align="center"
+          sx={{ display: { xs: "block", sm: "none" } }}
+        >
           <Logo />
         </Typography>
         <div
-          className='search-bar b-radius'
-          style={{ background: !mode ? '#212121' : '#fff' }}>
+          className="search-bar b-radius"
+          style={{ background: !mode ? "#212121" : "#fff" }}
+        >
           <TextField
-            label='Search'
-            variant='standard'
+            label="Search"
+            variant="standard"
             value={search}
             onChange={(event) => setSearch(event.target.value)}
           />
           <SearchIcon />
         </div>
         <IconButton
-          edge='end'
-          color='inherit'
-          aria-label='mode'
-          onClick={toggleTheme}>
+          edge="end"
+          color="inherit"
+          aria-label="mode"
+          onClick={toggleTheme}
+        >
           {icon}
         </IconButton>
         {/* <IconButton color='inherit'>
@@ -78,18 +83,18 @@ const TopNav = ({
             <NotificationsOutlinedIcon />
           </Badge>
         </IconButton> */}
-        <Avatar sx={{ bgcolor: '#f77062' }}>UN</Avatar>
+        <Avatar sx={{ bgcolor: "#f77062" }}>UN</Avatar>
         <Typography
-          component='h2'
-          variant='h6'
-          className='letter-spacing'
-          sx={{ display: { xs: 'none', sm: 'block' } }}>
+          component="h2"
+          variant="h6"
+          className="letter-spacing"
+          sx={{ display: { xs: "none", sm: "block" } }}
+        >
           User's Name
         </Typography>
       </Toolbar>
     </MuiAppBar>
-  )
-}
+  );
+};
 
-export default TopNav
-
+export default TopNav;
