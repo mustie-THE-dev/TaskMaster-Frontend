288880e59e83e1ba7aaa0e2a21f9a09c7d605a4f
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-11 13:01:42 +0300

    worked on creating the newboard criteria

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,8 +1,8 @@
-import * as React from 'react'
-import DropdownMenu from '../DropdownMenu'
-import BoardModal from '../modal/BoardModal'
-import TaskModal from '../modal/TaskModal'
-import Tasks from '../../containers/Tasks'
+import * as React from "react";
+import DropdownMenu from "../DropdownMenu";
+import BoardModal from "../modal/BoardModal";
+import TaskModal from "../modal/TaskModal";
+import Tasks from "../../containers/Tasks";
 import {
   Grid,
   Typography,
@@ -10,9 +10,9 @@ import {
   Tooltip,
   IconButton,
   Button,
-} from '@mui/material'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
-import AddIcon from '@mui/icons-material/Add'
+} from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import AddIcon from "@mui/icons-material/Add";
 
 const Board = ({
   board,
@@ -23,31 +23,31 @@ const Board = ({
   colors,
   mode,
 }) => {
-  const { name, id } = board
-  const [tasks, setTasks] = React.useState([])
+  const { name, id } = board;
+  const [tasks, setTasks] = React.useState([]);
 
   React.useEffect(() => {
-    setTasks(board.tasks)
-  }, [board])
+    setTasks(board.tasks);
+  }, [board]);
 
   //board menu to see more options
-  const [moreAnchorEl, setMoreAnchorEl] = React.useState(null)
-  const isMenuOpen = Boolean(moreAnchorEl)
+  const [moreAnchorEl, setMoreAnchorEl] = React.useState(null);
+  const isMenuOpen = Boolean(moreAnchorEl);
 
   const handleMenuOpen = (event) => {
-    setMoreAnchorEl(event.currentTarget)
-  }
+    setMoreAnchorEl(event.currentTarget);
+  };
 
   const handleMenuClose = () => {
-    setMoreAnchorEl(null)
-  }
-   // handle new task
-   const handleCreateTask = (newTask) => {
-    fetch('/api/tasks/', {
-      method: 'POST',
+    setMoreAnchorEl(null);
+  };
+  // handle new task
+  const handleCreateTask = (newTask) => {
+    fetch("/api/tasks/", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        accept: 'application/json',
+        "Content-Type": "application/json",
+        accept: "application/json",
       },
       body: JSON.stringify({
         board_id: id,
@@ -62,47 +62,47 @@ const Board = ({
       .then((res) => res.json())
       .then((data) => {
         if (tasks.length === 0) {
-          setTasks([data])
+          setTasks([data]);
         } else {
           setTasks((prevTasks) => {
-            return [...prevTasks, data]
-          })
+            return [...prevTasks, data];
+          });
         }
-      })
-  }
+      });
+  };
 
   //handle edit board modal
-  const [openBoardModal, setOpenBoardModal] = React.useState(false)
-  const handleOpenBoardModel = () => setOpenBoardModal(true)
-  const handleCloseBoardModel = () => setOpenBoardModal(false)
-
+  const [openBoardModal, setOpenBoardModal] = React.useState(false);
+  const handleOpenBoardModel = () => setOpenBoardModal(true);
+  const handleCloseBoardModel = () => setOpenBoardModal(false);
 
   //handle create task modal
-  const [openTaskModal, setOpenTaskModal] = React.useState(false)
-  const handleOpenTaskModel = () => setOpenTaskModal(true)
-  const handleCloseTaskModel = () => setOpenTaskModal(false)
-
+  const [openTaskModal, setOpenTaskModal] = React.useState(false);
+  const handleOpenTaskModel = () => setOpenTaskModal(true);
+  const handleCloseTaskModel = () => setOpenTaskModal(false);
 
   return (
     <Grid
       item
-      className='board b-radius-sm'
-      flexDirection='column'
+      className="board b-radius-sm"
+      flexDirection="column"
       sx={{
         backgroundColor: (theme) =>
           mode ? colors.colorLight : theme.palette.grey[800],
-      }}>
-      <Grid container alignContent='center' justifyContent='space-between'>
-        <Typography variant='h6' component='h3' gutterBottom>
+      }}
+    >
+      <Grid container alignContent="center" justifyContent="space-between">
+        <Typography variant="h6" component="h3" gutterBottom>
           {name}
         </Typography>
-        <Box className='flex'>
-          <Tooltip title='Board Options'>
+        <Box className="flex">
+          <Tooltip title="Board Options">
             <IconButton
-              aria-label='show options'
-              aria-controls='board-options'
-              aria-haspopup='true'
-              onClick={handleMenuOpen}>
+              aria-label="show options"
+              aria-controls="board-options"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+            >
               <MoreHorizIcon />
             </IconButton>
           </Tooltip>
@@ -120,27 +120,27 @@ const Board = ({
 
       {/* add task button */}
       <Button
-        className='btn-add'
-        color='inherit'
+        className="btn-add"
+        color="inherit"
         onClick={handleOpenTaskModel}
         startIcon={<AddIcon />}
-        variant='outlined'>
+        variant="outlined"
+      >
         Add Task
       </Button>
 
-       {/* popups */}
-       <DropdownMenu
+      {/* popups */}
+      <DropdownMenu
         moreAnchorEl={moreAnchorEl}
         isMenuOpen={isMenuOpen}
         handleMenuClose={handleMenuClose}
         handleOpenModel={handleOpenBoardModel}
         handleDelete={handleDeleteBoard}
         component={board}
-        componentType='Board'
+        componentType="Board"
       />
 
-
-<BoardModal
+      <BoardModal
         board={board}
         openModal={openBoardModal}
         handleCloseModel={handleCloseBoardModel}
@@ -158,7 +158,7 @@ const Board = ({
         handleCreateTask={handleCreateTask}
       />
     </Grid>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
