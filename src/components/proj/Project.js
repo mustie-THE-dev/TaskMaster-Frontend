40c6ad8a6403e31bd7a4fdd0a0e38d96ffa52e54
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-11 13:34:08 +0300

    finished working on the modal

--- src/components/proj/Project.js
+++ src/components/proj/Project.js
@@ -49,3 +49,76 @@ const Project = ({
   const handleMenuClose = () => {
     setMoreAnchorEl(null)
   }
+  
+  const handleFavoringAProject = () => {
+    const updatefavProject = { ...project, favorite: !project.favorite }
+
+    handleUpdatingProject(updatefavProject)
+  }
+
+  //handle edit modal
+  const [openModal, setOpenModal] = React.useState(false)
+  const handleOpenModel = () => setOpenModal(true)
+  const handleCloseModel = () => setOpenModal(false)
+
+  return (
+    <Grid item xs={12} sm={12} md={6} lg={4}>
+      <Card
+        className='b-radius project-card'
+        style={{ background: project.color, color: '#444' }}>
+        <CardActionArea className='card-actions'>
+          <Tooltip
+            title={project.favorite ? 'Remove Favorite' : 'Add Favorite'}>
+            <IconButton
+              onClick={handleFavoringAProject}
+              style={{ color: '#444' }}>
+              {project.favorite ? <StarIcon /> : <StarBorderIcon />}
+            </IconButton>
+          </Tooltip>
+          <Tooltip title='Project Options'>
+            <IconButton
+              style={{ color: '#444' }}
+              aria-label='show options'
+              aria-controls='project-options'
+              aria-haspopup='true'
+              onClick={handleMenuOpen}>
+              <MoreVertIcon />
+            </IconButton>
+          </Tooltip>
+        </CardActionArea>
+        <CardContent>
+          <Link to={`/projects/${project.id}`} className='link'>
+            <Typography variant='h6' component='p'>
+              {project.title}
+            </Typography>
+            <LinearProgressWithLabel
+              value={progress}
+              barColor={currentColorScheme.color}
+            />
+          </Link>
+        </CardContent>
+      </Card>
+
+      {/* pop ups */}
+      <DropdownMenu
+        moreAnchorEl={moreAnchorEl}
+        isMenuOpen={isMenuOpen}
+        handleMenuClose={handleMenuClose}
+        handleOpenModel={handleOpenModel}
+        handleDelete={handleDeleteProject}
+        component={project}
+        componentType='Project'
+      />
+
+      <ProjectModal
+        project={project}
+        openModal={openModal}
+        handleCloseModel={handleCloseModel}
+        handleUpdatingProject={handleUpdatingProject}
+        mode={mode}
+      />
+    </Grid>
+  )
+}
+
+export default Project
