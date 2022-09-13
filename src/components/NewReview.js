a16d74174749651e749bb1e4ca85d018e68a3327
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-13 11:51:44 +0300

    done with review method

---
+++ src/components/NewReview.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <>
+        <nav>
+            <div className='div-header'>
+                <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                    <h2>PokeViewer</h2>
+                </div>
+                <div style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                    <Link to="/pokemon" className='btn'>
+                        Pokemon Feed
+                    </Link>
+                    <Link to="/" className='btn'>
+                        About
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default  Navbar;
