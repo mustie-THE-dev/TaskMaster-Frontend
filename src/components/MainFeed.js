129979628217fd53c3ff1097aa7d29e17ffad1ac
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-13 11:01:02 +0300

    worked on the main feed

---
+++ src/components/MainFeed.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+function MainFeed( {pokemons} ) {
+
+
+  return(
+    <div>
+      <ul style={{ display: "flex", flexWrap: "wrap" }}>
+      {pokemons.length > 0 ? (
+        pokemons.map((pokemon) => (
+          <div className='card'>
+            <article>
+              <h1>{pokemon.name}</h1>
+              <p><strong>Location:</strong> <em>{pokemon.location}</em></p>
+              <p><strong>Signature Move:</strong> <em>{pokemon.move}</em></p>
+            </article>
+            <Link className='btn' to={`/pokemon/${pokemon.id}`}>View</Link>
+          </div>
+        ))
+      ) : (
+        <>
+        <p>Nothing here</p>
+        </>
+      )}
+      </ul>
+    </div>
+  );
+}
+
+export default MainFeed;
+
