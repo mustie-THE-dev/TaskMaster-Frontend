1b3c459bc9b52b05480c801e3dab560f992b1c36
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-13 10:53:34 +0300

    description of the app updated

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Welcome to TaskMaster Pokemon</h1>
+        <p>A simple application that displays a list of the first 151 original pokemon.</p>
+        <img src='https://encrypted-tbn0.gstatic.com/images?aaq=tbn:ANd9GcQNGlrkaN4TBScT0kU0RBaRnfJVvqVAWa-Zl4FmCI2zVz1L75N-DKSvGnZX3NG9OCyPkrM&usqp=CAU'></img>
+    </div>
+  )
+}
+
+export default Home
